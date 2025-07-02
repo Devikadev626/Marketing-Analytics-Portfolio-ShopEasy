c3070db68da8d51e4c8576acc50ee8700de10de3
Devika M <123802643+Devikadev626@users.noreply.github.com>
2025-07-02 16:35:37 +0530

    Add files via upload

---
+++ Data/dim_customers.sql
@@ -0,0 +1,23 @@
+select * from customers;
+
+select * from geography;
+
+-- SQL statement to join dim_customers with dim_geography to enrich customer data with geographic information
+
+SELECT 
+    c.CustomerID,  
+    c.CustomerName,  
+    c.Email,  
+    c.Gender, 
+    c.Age,  
+    g.Country, 
+    g.City 
+FROM 
+    dbo.customers as c  
+LEFT JOIN
+-- RIGHT JOIN
+-- INNER JOIN
+-- FULL OUTER JOIN
+    dbo.geography g  
+ON 
+    c.GeographyID = g.GeographyID;  
